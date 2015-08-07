38905e2c85e087315195a6dd906d1cfec0849d69
Dan <dchen12812@gmail.com>
2015-08-06 17:39:26 -0700

    bug fix for javascript and dropdown menu

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
+  get 'destinations/nyc' => 'destinations#nyc', as: 'nyc'
+  get 'destinations/mxp' => 'destinations#mxp', as: 'mxp'
+  get 'destinations/nrt' => 'destinations#nrt', as: 'nrt'
+  get 'destinations/sfo' => 'destinations#sfo', as: 'sfo'
+  get 'destinations/prg' => 'destinations#prg', as: 'prg'
+
 end
