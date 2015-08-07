b1cfcb1a60735114c93a39979fa56eed5539242c
Dan <dchen12812@gmail.com>
2015-08-06 17:40:48 -0700

    added partials, javascript fixes, and init destinations controller

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,9 @@
+//Setup the modals
+$(document).ready(function(){
+    $("#signup").click(function(){
+        $("#signup-modal").modal();
+    });
+    $("#login").click(function(){
+        $("#login-modal").modal();
+    });
+});
---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,22 @@
+class DestinationsController < ApplicationController
+	def nyc
+
+	end
+
+	def prg
+
+	end
+
+	def nrt
+
+	end
+
+	def sfo
+
+	end
+
+	def mxp
+
+	end
+
+end
