f0c47ee4e17e7c2f91cbc0acb2d67dedfcfae9af
Dan <dchen12812@gmail.com>
2015-08-09 15:08:17 -0700

    bugfix on flash for search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,6 +8,8 @@ class SearchesController < ApplicationController
 	private
 	def logged_in?
 		redirect_to(root_url) unless signed_in?
-		flash[:notice] = "You need to be signed in to view that content"
+		unless signed_in?
+			flash[:notice] = "You need to be signed in to view that content"
+		end
 	end
 end
