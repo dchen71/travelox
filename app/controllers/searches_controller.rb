2381ff00eb00f06b9df7630a0860892d88b126a8
Dan <dchen12812@gmail.com>
2015-08-09 01:59:50 -0700

    added redirect login for search page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,13 @@
 class SearchesController < ApplicationController
+	before_action :logged_in?, only: [:search]
+
 	def search
 
+	end
+
+	private
+	def logged_in?
+		redirect_to(root_url) unless signed_in?
+		flash[:notice] = "You need to be signed in to view that content"
 	end
 end
