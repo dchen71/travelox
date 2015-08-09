44935c128eaf0b11ae48324042956c1cc925a926
Dan <dchen12812@gmail.com>
2015-08-09 01:18:38 -0700

    added base devise create functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < Devise::SessionsController  
-	clear_respond_to  
-    respond_to :json
+  clear_respond_to  
+  respond_to :json
+
+  # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
 end  
