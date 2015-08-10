d05ee76ff32a6180ebd66d8d1fa75d45a45ce59f
Dan <dchen12812@gmail.com>
2015-08-09 19:17:17 -0700

    ajax login done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,6 @@ class SessionsController < Devise::SessionsController
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    return render :json => {success: true, location: after_sign_in_path_for(resource)}
   end
 end  
