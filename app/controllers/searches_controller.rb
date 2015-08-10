a5447793bf585c674793c7dbaf6a9857e57b3ce9
Dan <dchen12812@gmail.com>
2015-08-09 20:47:27 -0700

    heroku commit

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,11 @@ class SearchesController < ApplicationController
 	before_action :logged_in?, only: [:search]
 
 	def search
+	end
 
+	def query
+		@response = Net::HTTP.get_response("example.com","/?search=thing&format=json")
+		return render :json => {success: true, reponse: @reponse}
 	end
 
 	private
