4fe40d638d101781c8ba41a5f6f4a21e1b653460
Dan <dchen12812@gmail.com>
2015-08-27 21:20:28 -0700

    added destinations controller test

--- test/controllers/destinations_controller_test.rb
+++ test/controllers/destinations_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class DestinationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@dest = destinations(:nyc)
+  end
+
+  test 'should show nyc' do
+  	get :show, code: @dest.code
+  	assert_response :success
+  end
 end
