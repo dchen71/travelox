b87fd89de72521a004257ada463e068d61291181
Dan <dchen12812@gmail.com>
2015-08-27 23:12:02 -0700

    added base test for password controller

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PasswordsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = users(:user1)
+  end
+
+  test "should follow a user with Ajax" do
+  	@request.env["devise.mapping"] = Devise.mappings[:user]
+  	post :create, format: 'json'
+  	assert_response :success
+  end
 end
