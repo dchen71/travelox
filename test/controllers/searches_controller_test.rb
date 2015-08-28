5f3bcad297479dd0ca0defdf92469c98d69e4382
Dan <dchen12812@gmail.com>
2015-08-27 23:55:51 -0700

    added tests for searches controller

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SearchesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = users(:user1)
+  end
+
+  test 'redirect if not logged in' do
+  	get :search
+  	assert_redirected_to root_path
+  end
+
+  test 'should be able to search' do
+  	sign_in @user
+  	get :search, format: 'json', start: 'France', dest: 'Germany'
+  	assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal true, body["success"]
+    assert body["response"].length > 0
+  end
 end
