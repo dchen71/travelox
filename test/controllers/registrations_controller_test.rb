1824a1d16d53e68422125f4eef5e08bd49f0e346
Dan <dchen12812@gmail.com>
2015-08-27 23:42:00 -0700

    added test to user controller

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class RegistrationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = users(:user1)
+  end
+
+  test "should create new user" do
+  	@request.env["devise.mapping"] = Devise.mappings[:user]
+  	post :create, format: 'json', user:{email: 'crab@kani.com', password: 'password', password_confirmation: 'password'}
+  	assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal true, body["success"]
+  end
 end
