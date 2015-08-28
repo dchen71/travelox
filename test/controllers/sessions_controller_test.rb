abe41a033aab3aee97905bdc4961a9cec26f5413
Dan <dchen12812@gmail.com>
2015-08-28 00:49:26 -0700

    added base code for sessions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = users(:user1)
+  end
+
+  test 'should be able to login' do
+  	@request.env["devise.mapping"] = Devise.mappings[:user]
+  	#post :create, format: 'json', user: {email: @user.email, password: 'password'}
+  	#assert_response :success
+    #body = JSON.parse(response.body)
+    #assert_equal true, body["success"]
+  end
 end
